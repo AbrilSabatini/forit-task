f2d6219dfd7ac85831d864af3108284df27cdf16
AbrilSabatini <abrilsabatini2004@gmail.com>
2025-06-23 12:47:00 -0300

    feat: add CORS to the backend

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -1,9 +1,11 @@
+import cors from "cors";
 import express from "express";
 import taskRouter from "./routes/task.routes";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(express.json());
 app.use("/api", taskRouter);
 
